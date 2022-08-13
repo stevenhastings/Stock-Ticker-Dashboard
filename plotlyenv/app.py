ec7c43e3ea5e3597b77172acb81a05122367275e
Steven Hastings <steve.hastings0321@gmail.com>
2022-08-13 17:49:07 -0400

    first 10k

---
+++ plotlyenv/app.py
@@ -0,0 +1,22 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+# Launch application
+app = dash.Dash()
+
+# Div containing [headers, input box, graph]
+app.layout = html.Div([
+                        html.H1('Stock Ticker Dashboard'),
+                        html.H3('Enter a stock symbol:'),
+                        
+                        dcc.Input(id= 'my_stock_picker',
+                                  value= 'TSLA'),
+                        dcc.Graph(
+                                  id= 'my_graph',
+                                  figure= {
+                                            'data': [{
+                                            'x': [1,2], 
+                                            'y': [3,1]
+                                                     }]
+                                           
